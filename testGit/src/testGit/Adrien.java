5273e7b7ef939414498a6f6f482c3a36da8444f9
Emilie GOUDEAU <emiliegoudeau.eg@gmail.com>
2017-04-03 15:00:47 +0200

    Mise à jour du 03/04/2017 14h50 par Emilie On va voir si ça marche

--- testGit/src/testGit/Adrien.java
+++ testGit/src/testGit/Adrien.java
@@ -2,4 +2,13 @@ package testGit;
 
 public class Adrien {
 
+	
+	protected String nom = "OUAIRY";
+	protected String prenom = "Adrien";
+	
+	
+	public String getPresentation() {
+		return "Je m'appelle " + nom + " " + prenom;
+		
+	}
 }
