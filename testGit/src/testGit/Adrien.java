5069b721f36bee90ff9307e878fbbbfbee2cd6d7
Adouairy <adrien.ouairy@wanadoo.fr>
2017-04-03 14:46:53 +0100

    Mise a jour du 03/04/2017 14h46 pleins de belles modifications

--- testGit/src/testGit/Adrien.java
+++ testGit/src/testGit/Adrien.java
@@ -2,4 +2,11 @@ package testGit;
 
 public class Adrien {
 
+	protected Integer taille = 183;
+	protected String nom = "Ouairy";
+	protected String prenom = "Adrien";
+
+	protected String presentationAdrien() {
+		return "Bonjour je m'appel " + prenom + " " + nom + " et je mesure " + taille + "cm !";
+	}
 }
