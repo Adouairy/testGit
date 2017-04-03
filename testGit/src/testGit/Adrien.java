01c4adeed68d30ff36fab1f9013965befad6a50b
Adouairy <adrien.ouairy@wanadoo.fr>
2017-04-03 16:01:52 +0100

    ajout des versionning

--- testGit/src/testGit/Adrien.java
+++ testGit/src/testGit/Adrien.java
@@ -2,16 +2,11 @@ package testGit;
 
 public class Adrien {
 
-
 	protected String nom = "Ouairy";
 	protected String prenom = "Adrien";
 	protected Integer age;
-	
-	
+
 	public String getPresentation() {
 		return "Je m'appelle " + nom + " " + prenom;
-		
-
-	
 	}
 }
